fe4e0a42e6c4d04fb37f2123447965277239b6aa
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-04-10 10:54:15 +0100

    test(green): /GET pokemonAPI should return a list of pokemon. DTO used for API response

--- src/infrastructure/api/pokemon.api.ts
+++ src/infrastructure/api/pokemon.api.ts
@@ -1,10 +1,15 @@
 import { Pokemon } from "../../domain/models/Pokemon";
+import { PokemonListDTO } from "../DTO/PokemonListDTO";
 
 export const pokemonApi = {
     getPokemonList: async (): Promise<Pokemon[]> => {
         // Aqui se hace la llamada a la API
         // Cuando se haga la llamada se ha de recibir el DTO y se transforma
         
-        return []
+        const url = "http://127.0.0.1:3000/api/pokemon";
+        const response = await fetch(url);
+        const data: PokemonListDTO = await response.json();
+        
+        return data.PokemonList
     }
 }
