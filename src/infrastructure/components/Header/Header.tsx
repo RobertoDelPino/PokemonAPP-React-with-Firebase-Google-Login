b9a1224c71b32a4c3ca92a207cb186a3ef10bfa3
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-04-20 13:07:03 +0100

    Header.tsx test(green): header should have "Log In" text button if userId is null. Check if user is null and then show a button text or another

--- src/infrastructure/components/Header/Header.tsx
+++ src/infrastructure/components/Header/Header.tsx
@@ -3,10 +3,14 @@ import * as firebase from "../../api/Firebase/Login.api";
 
 export function Header(props) {
 
+    const {userId} = props
     return (
         <>
             <header>
-
+                { userId ?
+                    <button onClick={firebase.logout}>Log Out</button>
+                        : <button onClick={firebase.logout}>Log In</button>
+                }
             </header>
         </>
     );
