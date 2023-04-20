b05095352b9d02989427027503149245d3c4dcbf
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-04-20 07:52:41 +0100

    deleted: FavoritesPokemon.services.ts who save data in localStorage

--- src/domain/services/FavoritesPokemonServices/LocalStorage/FavoritesPokemon.services.jsdom.test.ts
+++
@@ -1,9 +0,0 @@
-import {FavoritesPokemonServices} from "./FavoritesPokemon.services";
-import {Pokemon} from "../../../models/Pokemon";
-
-describe("it", () => {
-    it('should ', function () {
-        const pokemon = new Pokemon(1, "", 1, 1, "")
-        FavoritesPokemonServices.setFavoritesPokemon(pokemon)
-    });
-})
